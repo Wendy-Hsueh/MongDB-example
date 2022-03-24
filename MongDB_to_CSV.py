5e836a7b83f2c361dcb97abb8084a7f4d419224e
Wendy Hsueh <wendyhsueh28@gmail.com>
2022-03-24 23:42:04 +0800

    Add files via upload

---
+++ MongDB_to_CSV.py
@@ -0,0 +1,21 @@
+from pymongo import MongoClient
+# import pandas as pd
+import datetime, csv, codecs
+import time, jieba
+
+_client = MongoClient('localhost:27017',username='Your_username',password='Your_password',authMechanism='MONGODB-CR')
+_db = _client.eoc
+_message = _db.message
+
+i = 0
+cursor = _message.find()
+with codecs.open('data.csv', 'w', 'utf-8-sig') as outputfile:
+	writer = csv.writer(outputfile)
+	writer.writerow(['title', 'content', 'time'])
+	for data in cursor:
+		writer.writerow([data['title'], data['content'], data['datetime']])
+		i+=1
+		if i == 200:
+			break
+
+_client.close()
