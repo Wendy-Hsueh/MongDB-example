38173bc77c5493b37cbeec7a2ae36464c338203f
Wendy Hsueh <wendyhsueh28@gmail.com>
2022-03-25 13:23:58 +0800

    Update MongDB_TimeRange.py

--- MongDB_TimeRange.py
+++ MongDB_TimeRange.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 import datetime, csv, codecs
 import time, jieba
 
-_client = MongoClient('localhost:27017',username='Your_username',password='Your_password',authMechanism='MONGODB-CR')
+_client = MongoClient('localhost',username='Your_username',password='Your_password',authMechanism='MONGODB-CR')
 _db = _client..Your_db_name
 _message = _db..Your_table_name
 timeRange = {'$gte': '2021-10-14 00:00', '$lt': '2021-10-15 12:00'}
